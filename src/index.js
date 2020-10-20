84dd68f05842290d1c63e9c2470b6e205b235fba
mariyafrantsava <mariya.frantsava7@gmail.com>
2020-10-20 22:42:07 +0300

    init: start and finish task brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let flag = true;
+    let count = 0;
+    do {
+        count = 0;
+        for (let i = 0; i < bracketsConfig.length; i++) {
+            brackets = bracketsConfig[i][0] + bracketsConfig[i][1];
+            position = str.indexOf(brackets);
+            str = str.split(brackets).join('');
+            if (position >= 0) {
+                count++;
+            }
+        }
+    } while (count > 0);
+
+    if (str.length > 0) {
+        return false;
+    } else {
+        return true;
+    }
 }
